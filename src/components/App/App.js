e9dcf62352cdba13d4d815efb5ab24a3f1e8e574
Jan Handzel <jhandzel130696@gmail.com>
2021-06-29 21:08:07 +0200

    zadanie 1 wykorzystanie propsow

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-
+import styles from './App.scss';
+import List from '../List/List';
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>My first React app</h1>
-      </div>
+      
+        <main className={styles.component}>
+        <h1 className ={styles.title}>My first React a</h1>
+        <h2 className = {styles.subtitle}>Hello World</h2>
+        <List title={['Things to do',<sup key='1'>soon!</sup>]} 
+              img='https://i.postimg.cc/c1tbWtjn/pexels-george-harrell-4157471.jpg'>
+              
+        </List>
+        
+        
+      </main>
     )
   }
 }
